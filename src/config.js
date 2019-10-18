0c5d950662db835a30c44ef18109cab0a795233b
krill <lpmaze@gmail.com>
2019-10-18 17:05:24 -0400

    update

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 export default {
-  API_ENDPOINT: "http://localhost:8000/api",
-  TOKEN_KEY: "blogful-client-auth-token"
+  // API_ENDPOINT: "http://localhost:8000/api",
+  // TOKEN_KEY: "blogful-client-auth-token"
+  API_ENDPOINT: 'https://mighty-harbor-79237.herokuapp.com/api',
+  TOKEN_KEY: '4b067e62-21d6-48f1-936b-c1d5591a4e14'
 };
